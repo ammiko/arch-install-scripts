000640ad129113adebecdcac8f52577e0a459820
ammiko <ammiko@tuta.io>
2020-11-21 15:26:29 +0530

    final edits

--- chroot_install.sh
+++ chroot_install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# get the variables from the user
+read ROOT
+passwd
+read USER
+read -s PASSWORD
 # configuring locale settings
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
@@ -8,18 +13,21 @@ sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 # setting up username
-echo $USER > /etc/hostname
+echo $ROOT > /etc/hostname
 echo "127.0.0.1    localhost
 ::1          localhost
-127.0.1.1    $USER.localdomain $USER" > /etc/hosts
+127.0.1.1    $ROOT.localdomain $ROOT" > /etc/hosts
 # installing grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 # setting up home user
 useradd -mG wheel $USER
-passwd $USER
 echo $USER:$PASSWORD | chpasswd
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 # enabling services
 systemctl enable NetworkManager
-systemctl enable bluetoothd
+systemctl enable bluetooth
+# finishing installation
+exit
+umount -a
+reboot
--- pre_install.sh
+++ pre_install.sh
@@ -1,34 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# get the variables from the user
-read USER
-read -s PASSWORD
 # set up time-date and repos
 pacman -Syyy
 timedatectl set-ntp true
-# partioning and formating disks
-
-#mkfs.fat -F32 /dev/sda1
-#mkfs.ext4 /dev/sda2
-#mount /dev/sda2 /mnt
-#mkdir /mnt/boot
-#mount /dev/sda1 /mnt/boot
 # installing software
-pacstrap /mnt base base-devel linux-zen 
+pacstrap /mnt base base-devel linux linux-headers
 pacstrap /mnt grub efibootmgr networkmanager 
 pacstrap /mnt pulsemixer playerctl linux-firmware
-pacstrap /mnt git dialog kitty intel-ucode stow 
-pacstrap /mnt bluez bluez-utils dialog mpv-mpris
-pacstrap /mnt zsh zsh-syntax-highlighting zsh-autosuggestions
-pacstrap /mnt nvim pulseaudio firefox zathura nnn
+pacstrap /mnt git alacritty intel-ucode stow
+pacstrap /mnt bluez bluez-utils dialog kakoune
+pacstrap /mnt zsh zsh-syntax-highlighting dialog
+pacstrap /mnt pulseaudio firefox zsh-autosuggestions
 pacstrap /mnt gvfs-afc gvfs-mtp ffmpegthumbnailer 
-pacstrap /mnt lua mpv ttf-dejavu python python-pip
-pacstrap /mnt powerline-fonts wl-clipboard youtube-dl
-pacstrap /mnt zathura-djvu zathura-pdf-poppler mako 
+pacstrap /mnt mpv ttf-dejavu youtube-dl picom
+pacstrap /mnt powerline-fonts bspwm sxhkd mpv
+pacstrap /mnt zathura-djvu zathura-pdf-poppler 
 # generating fs and chrooting
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash ./chroot_install.sh $USER $PASSWORD
-# finishing installation
-exit
-umount -a
-reboot
