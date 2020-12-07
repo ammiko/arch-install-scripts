0f801d2393f2be03a6bfb411e524afff3fa82a2e
ammiko <ammiko@tuta.io>
2020-12-07 19:08:52 +0530

    added some more packages

--- pre_install.sh
+++ pre_install.sh
@@ -8,12 +8,13 @@ pacstrap /mnt base base-devel linux linux-headers
 pacstrap /mnt grub efibootmgr networkmanager 
 pacstrap /mnt pulsemixer playerctl linux-firmware
 pacstrap /mnt git alacritty intel-ucode stow
-pacstrap /mnt bluez bluez-utils dialog kakoune
-pacstrap /mnt zsh zsh-syntax-highlighting dialog
+pacstrap /mnt bluez bluez-utils dialog
+pacstrap /mnt zsh zsh-syntax-highlighting
 pacstrap /mnt pulseaudio firefox zsh-autosuggestions
 pacstrap /mnt gvfs-afc gvfs-mtp ffmpegthumbnailer 
 pacstrap /mnt mpv ttf-dejavu youtube-dl picom
 pacstrap /mnt powerline-fonts bspwm sxhkd mpv
-pacstrap /mnt zathura-djvu zathura-pdf-poppler 
+pacstrap /mnt zathura-djvu zathura-pdf-poppler
+pacstrap /mnt emacs cowfortune lolcat
 # generating fs and chrooting
 genfstab -U /mnt >> /mnt/etc/fstab
